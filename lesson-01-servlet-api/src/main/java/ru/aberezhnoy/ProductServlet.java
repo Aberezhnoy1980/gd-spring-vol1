28accf43d43c1631c8c3296f64105f04dd07a886
Aberezhnoy <Berezhnoy_aa@mail.ru>
2021-12-21 16:58:47 +0300

    homework done. Server application http://80.78.248.30:8080/app/product

--- lesson-01-servlet-api/src/main/java/ru/aberezhnoy/ProductServlet.java
+++ lesson-01-servlet-api/src/main/java/ru/aberezhnoy/ProductServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import static java.lang.Long.parseLong;
+
 @WebServlet(urlPatterns = "/product/*")
 public class ProductServlet extends HttpServlet {
 
@@ -26,8 +28,21 @@ public class ProductServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter wr = resp.getWriter();
         if (req.getParameter("id") != null) {
-            wr.println()
-            //TODO
+            long id = parseLong(req.getParameter("id"));
+            wr.println("<label><a href=product>К списку</a></label>");
+            wr.println("<h3>Product info: id - " + productRepository.findById(id).getId() +", name - " + productRepository.findById(id).getName() + "</h3>");
+
+            wr.println("<table>");
+            wr.println("<tr>");
+            wr.print("<th>Id</th>");
+            wr.print("<th>Name</th>");
+            wr.println("</tr>");
+            wr.println("<tr>");
+            wr.println("<td>" + productRepository.findById(id).getId() + "</td>");
+            wr.println("<td>" + productRepository.findById(id).getName() + "</td>");
+            wr.println("</tr>");
+            wr.println("</table>");
+
         } else if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
             wr.println("<table>");
 
@@ -38,8 +53,8 @@ public class ProductServlet extends HttpServlet {
 
             for (Product product : productRepository.findAll()) {
                 wr.println("<tr>");
-                wr.println("<td>" + product.getId() + "</td>");
-                wr.println("<td>" + product.getName() + "</td>");
+                wr.println("<td><a href=product?id=" + product.getId() + ">" + product.getId() + "</a></td>");
+                wr.println("<td><a href='product?id=" + product.getId() + "'>" + product.getName() + "</a></td>");
                 //TODO <a href='product?id=12'></a>
                 wr.println("</tr>");
             }
